49a3d75726bc902891917ad69efab7764561033b
fmording <fmo@hotmail.no>
2020-11-05 14:19:53 +0100

    Edit to editpage and tries on selectmenu

--- public/js/client.js
+++ public/js/client.js
@@ -8,3 +8,37 @@ btnAddTxt.addEventListener('click', async evt => {
     txt1.spawnText(document.getElementById('slideDiv'));
 
 })
+
+
+let presMenuSelect = document.getElementById('crntPresentation');
+let savePresBtn = document.getElementById('savePresBtn');
+
+
+
+savePresBtn.addEventListener('click', presTitle => {
+  //Code to check if title already exist, if so -> update presentation
+  //if there is no title written in input -> do nothing
+  //when presentation saved, add to list above and alert user "saved"
+  let newPres = document.getElementById('inpPresName').value;
+
+  let elm = document.createElement("option");
+  elm.innerHTML = `${newPres}`;
+  presMenuSelect.appendChild(elm);
+});
+// Dette skal kunne finne mouse position, men er obsolete.
+/*const slideDiv = document.getElementById('slideDiv');
+let userElm = document.getElementById('userText1'); //Will be variable activeElement after figuring out how to let this var be whatever user clicked
+
+let active = false;
+let pageX = 0;
+let pageY = 0;
+
+function getMousePos(evt) {
+  pageX = evt.pageX;
+  pageY = evt.pageY;
+}
+console.log(pageX + ', ' + pageY);
+
+slideDiv.addEventListener('mousedown', dragStart, false);
+slideDiv.addEventListener('mousemove', drag, false);
+slideDiv.addEventListener('mouseup', dragEnd, false);*/
--- public/js/spawningElement.js
+++ public/js/spawningElement.js
@@ -1,3 +1,4 @@
+let txtNmbr = 0;
 
 class TextBox {
     constructor (content) {
@@ -6,13 +7,16 @@ class TextBox {
 
     }
 
+    
+
     spawnText(slideDiv) {
         let elm = document.createElement("h3");
         elm.setAttribute('class', 'userAddedText');
         elm.setAttribute('contentEditable', 'true');
-        elm.setAttribute('draggable', 'true');
+        elm.setAttribute('id', 'userText' + txtNmbr);
         elm.textContent = this.content;
         slideDiv.appendChild(elm);
+        txtNmbr ++;
     }
 
 
