4f50b6226fee2f0c555c4fe4555834a04c7ff614
carob16 <44673055+carob16@users.noreply.github.com>
2020-11-09 14:02:57 +0100

    create presentation in DB function created in pool.js and called in server.js

--- db/pool.js
+++ db/pool.js
@@ -44,10 +44,30 @@ module.exports = {
     },
 
 
+    createPres: function(inp) {
+        let input = inp;
+        pool.query(`INSERT INTO presentations(userid, data)VALUES($1,$2) RETURNING presentid`, [input[0], input[1]], //input 1=userid, input 2 = data
+            function(err, result) {
+                if (err) {
+                    console.log(input[0]);
+                    console.log(err);
+                } else {
+                    console.log(input[2]);
+                    console.log('coliumn updated with prsentations: ' + result.rows[0].presentationid);
+                }
+                console.log('Client will end now!');
+                pool.end();
+
+            });
+        return result.rows[0].presentationid;
+
+    },
+
+
     updatePres: function(inp) {
 
         let input = inp;
-        pool.query(`UPDATE users SET Presentations =$1 WHERE id = $2 RETURNING id`, [input[1], input[0]],
+        pool.query(`UPDATE presentations SET data =$1 WHERE presentid = $2 RETURNING id`, [input[1], input[0]],
             function(err, result) {
                 if (err) {
                     console.log(input[0]);
