f747feceefafb571ec720fa23d6a1d1e61da7308
carob16 <44673055+carob16@users.noreply.github.com>
2020-11-09 15:00:27 +0100

    Crreating presentaion in DB works fine, trying to update presentation

--- db/pool.js
+++ db/pool.js
@@ -44,38 +44,39 @@ module.exports = {
     },
 
 
-    createPres: function(inp) {
+    createPres: async function(inp) {
         let input = inp;
         console.log(input, " ", input[0]);
+        let presentationArray = "";
         pool.query(`INSERT INTO presentations(userid, data)VALUES($1,$2) RETURNING presentationid`, [input[0], input[1]], //input 1=userid, input 2 = data
+
             function(err, result) {
                 if (err) {
-                    console.log(input[0]);
                     console.log(err);
                 } else {
-                    console.log(input[2]);
-                    console.log('coliumn updated with prsentations: ' + result.rows[0]);
+                    console.log('column updated with prsentations: ' + result.rows[0].data + ", presentationid = " + result.rows[0].presentationid);
                 }
                 console.log('Client will end now!');
+
+                presentationArray = [result.rows[0].presentationid, result.rows[0].data];
                 pool.end();
 
             });
-        // return result.rows[0].presentationid;
-
+        return presentationArray;
     },
 
 
-    updatePres: function(inp) {
+    updatePres: async function(inp) {
 
         let input = inp;
-        pool.query(`UPDATE presentations SET data =$1 WHERE presentid = $2 RETURNING id`, [input[1], input[0]],
+        pool.query(`UPDATE presentations SET data =$1 WHERE presentationid = $2 RETURNING id`, [input[1], input[0]],
             function(err, result) {
                 if (err) {
                     console.log(input[0]);
                     console.log(err);
                 } else {
                     console.log(input[1]);
-                    console.log('coliumn updated with prsentations: ' + result.rows[0].presentations);
+                    console.log('column updated with presentation: ' + result.rows[0].presentations + "for user" + result.rows[0].userid);
                 }
                 console.log('Client will end now!');
                 pool.end();
