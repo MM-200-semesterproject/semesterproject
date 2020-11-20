214c54f4fe5668369b93c02e0ebd18925a82d11f
FilipSzutkowski <filip99.ski@gmail.com>
2020-11-20 11:36:53 +0100

    Local db env working

---
+++ db/localenv.js
@@ -0,0 +1,6 @@
+const localEnv = {
+  credentials:
+    'postgres://zbarldkwlsxvrm:77cb1aa6430cce2eddceeffe6a86eff624bd91c7d075933fd9da2c57b66f467d@ec2-34-253-148-186.eu-west-1.compute.amazonaws.com:5432/d2gt3gcst21p49',
+};
+
+module.exports = localEnv;
