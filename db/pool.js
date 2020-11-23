61045093f8d6634c87d92ed81653b8bb29edac73
fmording <fmo@hotmail.no>
2020-11-23 14:05:59 +0100

    sort of conflicts in server and pool, this is the newest version according to filip

--- db/pool.js
+++ db/pool.js
@@ -1,122 +1,42 @@
 const pg = require('pg');
 const encrypt = require('./encryption.js');
 const dbCredentials =
-    process.env.DATABASE_URL || require('./localenv').credentials;
+  process.env.DATABASE_URL || require('./localenv').credentials;
 
 class StorageHandler {
-    constructor(credentials) {
-        this.credentials = {
-            connectionString: credentials,
-            ssl: {
-                rejectUnauthorized: false,
-            },
-        };
-    }
-
-    async newUser(body) {
-        const client = new pg.Client(this.credentials);
-        const input = encrypt.hashCode(body);
-        let results = null;
-        console.log(
-            `Pool.js: hashed input inside newUser: username: ${input.user.username}, password: ${input.user.password}`
-        );
-
-        try {
-            await client.connect();
-            results = await client.query(
-                'INSERT INTO users(Email, Password) VALUES($1, $2) RETURNING id', [input.user.username, input.user.password]
-            );
-            results = results.rows[0].id;
-            console.log(`row inserted with id: ${results}`);
-            client.end();
-        } catch (err) {
-            console.log(err);
-            client.end();
-        }
-    }
-
-    async loadUser(body) {
-        const client = new pg.Client(this.credentials);
-        const input = encrypt.hashCode(body);
-        let results = null;
-
-        try {
-            await client.connect();
-            results = await client.query(
-                'SELECT * FROM users WHERE email = $1 AND password = $2', [input.user.username, input.user.password]
-            );
-            results = results.rows[0].id;
-            console.log(`Result loadUser id: ${results}`);
-
-            client.end();
-        } catch (err) {
-            console.log(`Error on loadUser: ${err}`);
-            client.end();
-        }
-    }
-
-    async deleteUser(body) {
-        const client = new pg.Client(this.credentials);
-        const input = encrypt.singleHash(body);
-        let results = null;
-
-        try {
-            await client.connect();
-            results = await client.query(
-                'DELETE * FROM users WHERE id = $1 AND password = $2', [input.id, input.password]
-            );
-            userid = results.rows[0].id;
-            console.log(`user with id: ${userid} deleted`);
-
-            results = await client.query(
-                'DELETE * OUTPUT DELETED.presentationid FROM presentations WHERE userid = $1 ', [userid]
-            );
-            console.log(results);
-            console.log(`user with id: ${results} deleted`);
-
-            client.end();
-        } catch (err) {
-            console.log(`Error on deleteUser: ${err}`); //wrong in type login-page?
-            client.end();
-        }
-    }
-<<<<<<< HEAD
-
-    async loadPres(body) {
-        const client = new pg.Client(this.credentials);
-        const input = body;
-
-        let presentationArray = [];
-        let singlePres = '';
-        let results = null;
-
-        try {
-            await client.connect();
-            let results = await client.query(
-                'SELECT COUNT(userid) FROM presentations WHERE userid = $1', [input.user.id]
-            );
-
-            let rowcount = results.rows[0].count;
-
-            results = await client.query(
-                'SELECT * FROM presentations WHERE userid = $1', [input.user.id]
-            );
-            for (let i = 0; i < rowcount; i++) {
-                let row = results.rows[i];
-                presentationArray.push(row);
-            }
-            return presentationArray;
-
-            client.end();
-        } catch (err) {
-            console.log(`Error on loadPres: ${err}`);
-            client.end();
-        }
-=======
-    return results;
+  constructor(credentials) {
+    this.credentials = {
+      connectionString: credentials,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    };
   }
 
-  async loadUserCopy(body) {
+  async newUser(body) {
+    const client = new pg.Client(this.credentials);
+    const input = encrypt.hashCode(body);
+    let results = null;
+    console.log(
+      `Pool.js: hashed input inside newUser: username: ${input.user.username}, password: ${input.user.password}`
+    );
+
+    try {
+      await client.connect();
+      results = await client.query(
+        'INSERT INTO users(Email, Password) VALUES($1, $2) RETURNING id',
+        [input.user.username, input.user.password]
+      );
+      results = results.rows[0].id;
+      console.log(`row inserted with id: ${results}`);
+      client.end();
+    } catch (err) {
+      console.log(err);
+      client.end();
+    }
+  }
+
+  async loadUser(body) {
     const client = new pg.Client(this.credentials);
     const input = encrypt.hashCode(body);
     let results = null;
@@ -132,52 +52,116 @@ class StorageHandler {
 
       client.end();
     } catch (err) {
-      console.log(`Error on loadUser: ${err}`); //wrong in type login-page?
+      console.log(`Error on loadUser: ${err}`);
       client.end();
->>>>>>> ea3838963939b9e0f8cbaef6aecd14cafa4a28df
     }
+  }
 
-    async createPres(inp) {
-        const client = new pg.Client(this.credentials);
-        const input = inp;
-        console.log([input.userid, input.slides, input.title]);
-        let results = null;
+  async deleteUser(body) {
+    const client = new pg.Client(this.credentials);
+    const input = encrypt.singleHash(body);
+    let results = null;
 
-        try {
-            await client.connect();
-            results = await client.query(
-                'INSERT INTO presentations(userid, title, data)VALUES($1,$2, $3) RETURNING presentationid', [input.userid, input.title, input.slides] //input 1=userid, input 2 = data
-            );
+    try {
+      await client.connect();
+      results = await client.query(
+        'DELETE * FROM users WHERE id = $1 AND password = $2',
+        [input.id, input.password]
+      );
+      userid = results.rows[0].id;
+      console.log(`user with id: ${userid} deleted`);
 
-            results = results.rows[0];
-            console.log(`Row created with presentationID: ${results.presentationid}`);
-            console.log('Client will end now!');
-            client.end();
-        } catch (err) {
-            console.log(`Error on createPres: ${err}`);
-            client.end();
-        }
+      results = await client.query(
+        'DELETE * OUTPUT DELETED.presentationid FROM presentations WHERE userid = $1 ',
+        [userid]
+      );
+      console.log(results);
+      console.log(`user with id: ${results} deleted`);
+
+      client.end();
+    } catch (err) {
+      console.log(`Error on deleteUser: ${err}`); //wrong in type login-page?
+      client.end();
     }
+  }
 
-    async updatePres(inp) {
-        const client = new pg.Client(this.credentials);
-        let input = inp;
-        let result = null;
+  async loadPres(body) {
+    const client = new pg.Client(this.credentials);
+    const input = body;
 
-        try {
-            await client.connect();
-            result = await client.query(
-                'UPDATE presentations SET data =$1 WHERE presentationid = $2 RETURNING presentationid', [input[1], input[0]]
-            );
-            results = result.rows[0];
-            console.log(
-                `column updated with presentation: ${results.presentations} for user ${results.userid}`
-            );
-            client.end();
-        } catch (err) {
-            console.log(`Error in UpdatePres: ${err}`);
-        }
+    let presentationArray = [];
+    let singlePres = '';
+    let results = null;
+
+    try {
+      await client.connect();
+      let results = await client.query(
+        'SELECT COUNT(userid) FROM presentations WHERE userid = $1',
+        [input.user.id]
+      );
+
+      let rowcount = results.rows[0].count;
+
+      results = await client.query(
+        'SELECT * FROM presentations WHERE userid = $1',
+        [input.user.id]
+      );
+      for (let i = 0; i < rowcount; i++) {
+        let row = results.rows[i];
+        presentationArray.push(row);
+      }
+      return presentationArray;
+
+      client.end();
+    } catch (err) {
+      console.log(`Error on loadPres: ${err}`);
+      client.end();
     }
+  }
+
+  async createPres(inp) {
+    const client = new pg.Client(this.credentials);
+    const input = inp;
+    console.log([input.userid, input.slides, input.title]);
+    let results = null;
+
+    try {
+      await client.connect();
+      results = await client.query(
+        'INSERT INTO presentations(userid, title, data)VALUES($1,$2, $3) RETURNING presentationid',
+        [input.userid, input.title, input.slides] //input 1=userid, input 2 = data
+      );
+
+      results = results.rows[0];
+      console.log(`Row created with presentationID: ${results.presentationid}`);
+      console.log('Client will end now!');
+      client.end();
+    } catch (err) {
+      console.log(`Error on createPres: ${err}`);
+      client.end();
+    }
+  }
+
+  async updatePres(inp) {
+    const client = new pg.Client(this.credentials);
+    let input = inp;
+    let result = null;
+
+    try {
+      await client.connect();
+      result = await client.query(
+        'UPDATE presentations SET data =$1 WHERE presentationid = $2 RETURNING presentationid',
+        [input[1], input[0]]
+      );
+      results = result.rows[0];
+      console.log(
+        `column updated with presentation: ${results.presentations} for user ${results.userid}`
+      );
+      client.end();
+    } catch (err) {
+      console.log(`Error in UpdatePres: ${err}`);
+    }
+  }
 }
 
 module.exports = new StorageHandler(dbCredentials);
--- server.js
+++ server.js
@@ -11,36 +11,18 @@ const { loadUser } = require('./db/pool.js'); //Du trenger ikke denne her, bare 
 app.set('port', process.env.PORT || 8080);
 app.use(express.static('public'));
 app.use(
-    express.static(path.join(__dirname, 'public'), { index: 'login.html' })
+  express.static(path.join(__dirname, 'public'), { index: 'login.html' })
 );
 app.use(bodyParser.json());
 app.use(express.json());
 
 app.post('/presentation', (req, res) => {
-    let presentation = {
-        presentationid: req.body.presentationid,
-        title: req.body.title,
-        slides: req.body.slides,
-    };
+  let presentation = {
+    presentationid: req.body.presentationid,
+    title: req.body.title,
+    slides: req.body.slides,
+  };
 
-<<<<<<< HEAD
-    res.status(200).json(presentation);
-    return;
-});
-
-app.post('/signUp', async function(request, response) {
-    // Sends object to pool.js-->DB;
-    let result = await pool.newUser(request.body);
-    if (result instanceof Error) {
-        response.status(500).send(result + ' Try again');
-        return;
-    } else {
-        response.status(200).json({
-            msg: 'User created',
-        });
-        return;
-    }
-=======
   res.status(200).json(presentation);
   return;
 });
@@ -66,40 +48,43 @@ app.post('/login', async function (request, response) {
     response.status(200).json(result);
     return;
   }
->>>>>>> ea3838963939b9e0f8cbaef6aecd14cafa4a28df
 });
 
-app.post('/login', async function(request, response) {
-    let result = await pool.loadUser(request.body);
-    if (result instanceof Error) {
-        response.status(500).send(result + ' Try again');
-        return;
-    } else {
-        response.status(200);
-        return;
-    }
+app.post('/login', async function (request, response) {
+  let result = await pool.loadUser(request.body);
+  if (result instanceof Error) {
+    response.status(500).send(result + ' Try again');
+    return;
+  } else {
+    response.status(200);
+    return;
+  }
 });
 //---------------------test av pool.js functions--------------------------
 
 let testpresentation = {
-    userid: 40,
-    title: 'PresentationTitle',
-    slides: [{
-            text: 'text1',
-            img: 'https://image.shutterstock.com/image-photo/suraj-mukhi-flower-my-great-260nw-1467178388.jpg',
-            list: ['plane', 'boat', 'car'],
-        },
-        {
-            text: 'text2',
-            img: 'https://image.shutterstock.com/image-photo/suraj-mukhi-flower-my-great-260nw-1467178388.jpg',
-            list: ['1', '2', '3'],
-        },
-        {
-            text: 'text3',
-            img: 'https://image.shutterstock.com/image-photo/suraj-mukhi-flower-my-great-260nw-1467178388.jpg',
-            list: ['variables', 'letters', 'numbers'],
-        },
-    ],
+  userid: 40,
+  title: 'PresentationTitle',
+  slides: [
+    {
+      text: 'text1',
+      img:
+        'https://image.shutterstock.com/image-photo/suraj-mukhi-flower-my-great-260nw-1467178388.jpg',
+      list: ['plane', 'boat', 'car'],
+    },
+    {
+      text: 'text2',
+      img:
+        'https://image.shutterstock.com/image-photo/suraj-mukhi-flower-my-great-260nw-1467178388.jpg',
+      list: ['1', '2', '3'],
+    },
+    {
+      text: 'text3',
+      img:
+        'https://image.shutterstock.com/image-photo/suraj-mukhi-flower-my-great-260nw-1467178388.jpg',
+      list: ['variables', 'letters', 'numbers'],
+    },
+  ],
 };
 let testbody = { user: { id: 40 } };
 pool.createPres(testpresentation);
@@ -107,6 +92,6 @@ pool.loadPres(testbody);
 
 //---------------------test av pool.js functions ferdig --------------------------
 
-app.listen(app.get('port'), function() {
-    console.log('server running', app.get('port'));
+app.listen(app.get('port'), function () {
+  console.log('server running', app.get('port'));
 });
