2751ba761da6d3ed7cd0662a552ead69c32ce40e
carob16 <44673055+carob16@users.noreply.github.com>
2020-11-24 13:49:01 +0100

    encryption: check if != null added, editMode: started on API login and signup: added required usernaem and password

--- db/encryption.js
+++ db/encryption.js
@@ -1,35 +1,39 @@
 const pool = require('./pool.js');
 
-exports.hashCode = function(userData) {
-    let password = userData.password;
-    let username = userData.username;
+exports.hashCode = function (userData) {
+  let password = userData.password;
+  let username = userData.username;
 
+  if (username != null) {
     let returnObject = {
-        username: createHash(username),
-        password: createHash(password),
+      username: createHash(username),
+      password: createHash(password),
     };
 
     return returnObject;
+  } else {
+    return false;
+  }
 };
 
 function createHash(string) {
-    let hash = 0;
-    for (let i = 0; i < string.length; i++) {
-        let charOfString = string.charCodeAt(i);
-        hash = (hash << 5) - hash + charOfString; // <<5 shifts bits in binary 5 times left
-        hash |= 0; // Convert to 32bit integer  //bitwise OR assignment
-    }
-    return hash;
+  let hash = 0;
+  for (let i = 0; i < string.length; i++) {
+    let charOfString = string.charCodeAt(i);
+    hash = (hash << 5) - hash + charOfString; // <<5 shifts bits in binary
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
 }
 
-exports.singleHash = function(inpString) {
-    let string = inpString;
-    let hash = 0;
-    for (let i = 0; i < string.length; i++) {
-        let charOfString = string.charCodeAt(i);
-        hash = (hash << 5) - hash + charOfString;
-        hash |= 0;
-    }
+exports.singleHash = function (inpString) {
+  let string = inpString;
+  let hash = 0;
+  for (let i = 0; i < string.length; i++) {
+    let charOfString = string.charCodeAt(i);
+    hash = (hash << 5) - hash + charOfString;
+    hash |= 0;
+  }
 
-    return hash;
+  return hash;
 };
