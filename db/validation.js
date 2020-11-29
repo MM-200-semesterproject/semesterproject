178f7376f5e488178f5c5c42113d3951285d8979
FilipSzutkowski <filip99.ski@gmail.com>
2020-11-29 13:29:58 +0100

    ViewMode splitting to files done

--- db/validation.js
+++ db/validation.js
@@ -2,9 +2,6 @@ const regex = {
   email: /^[^\s]+@[^\s]+\.[^\s]+$/,
   password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, //Minimum eight characters, minimum one letter and one number:
 };
-//validation(email, password);
-
-//console.log(tst);
 
 exports.validate = function validation(email, password) {
   const testEmail = regex.email.test(email);
@@ -16,11 +13,3 @@ exports.validate = function validation(email, password) {
 
   return results;
 };
-/*
-
-check if email already excists in pool
-check if regex email is true Feil brukernavn, sjekk om du har skrevet brukernavnet riktig.
-check if regex password is true | Feil password, må inneholde minst en stor bokstav og 1 tall  og minst 8 tegn
-
-forbedringer for sikkerhet: sende confirmation email
-*/
